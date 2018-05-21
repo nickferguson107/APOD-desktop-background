946f8fc028bf9dffe28741beee0855b194f81e46
Nick <nickferguson107@icloud.com>
2018-05-21 19:39:07 +0100

    Updated update checking

--- APOD_DB.py
+++ APOD_DB.py
@@ -49,12 +49,13 @@ try:
     ctypes.windll.user32.SystemParametersInfoW(20, 0, str(image_path), 0)
     print("Image set as background.")
 except TypeError:
-    print("Update failed - APOD item is video. Waiting 1 day to update.")
-    time.sleep(84600)
+    print("Update failed - APOD item is video. Waiting 1 hour to update.")
+    time.sleep(3600)
 
 while True:
     current_time = datetime.datetime.now()
-    print("The current time is {}".format(str(current_time.time())))
+    formatted_time = datetime.datetime.strftime(current_time, "%H:%M:%S")
+    print("The current time is {}".format(str(formatted_time)))
     try:
         next_day_name, _ = get_image()
         if next_day_name == today_name:
@@ -62,10 +63,10 @@ while True:
             print("Waiting 10 minutes for next check.")
             time.sleep(600)
         else:
-            print("Updating at {}".format(str(current_time.time())))
+            print("Updating at {}".format(str(formatted_time)))
             job()
             time.sleep(43200)
             print("Waiting 12 hours before next check.")
     except TypeError:
-        print("Update failed - APOD item is video. Waiting 1 day to update.")
-        time.sleep(86400)
+        print("Update failed - APOD item is video. Waiting 1 hour to update.")
+        time.sleep(3600)
