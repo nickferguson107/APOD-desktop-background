61971f6bccf0d999bf229242617ce79873e3002c
Nick <nickferguson107@icloud.com>
2018-05-22 21:17:43 +0100

    Added classes

--- APOD_DB.py
+++ APOD_DB.py
@@ -6,46 +6,50 @@ import ctypes
 import sched
 from bs4 import BeautifulSoup
 
-def get_image():  
-    url = "http://apod.nasa.gov/"
-    page = urllib.request.urlopen(url)
-    soup = BeautifulSoup(page, "html.parser")
-    img = soup.find("img")
-    apod_img_name = str(img['src'])
-    apod_img_link = url + apod_img_name
-    apod_img_title = str(soup.find('b'))
-    today_name = apod_img_title[4:-5]
-    if ":" in today_name:
-        today_name = today_name.replace(":", " -", 1)
-    image_to_save = urllib.request.urlopen(apod_img_link).read()
-    return today_name, image_to_save
+class DesktopBackground:
 
-def save_image(name, image):
-    home_dir = os.path.expanduser('~')
-    today_date = str(datetime.date.today())
-    filename = today_date + " " + name + ".jpg"
-    save_path = os.path.join(home_dir, "Pictures", "APOD Photos")
-    save_path_with_img = os.path.join(save_path, filename)
-    normed_path = os.path.normpath(save_path)
-    normed_path_with_img = os.path.normpath(save_path_with_img)
-    if not os.path.exists(normed_path):
-        os.makedirs(normed_path)
-    with open(normed_path_with_img, 'wb',) as f:
-        f.write(image)
-    f.close()
-    return normed_path_with_img, today_date
+    def get_image(self):  
+        self.url = "http://apod.nasa.gov/"
+        self.page = urllib.request.urlopen(self.url)
+        self.soup = BeautifulSoup(self.page, "html.parser")
+        self.img = self.soup.find("img")
+        self.apod_img_name = str(self.img['src'])
+        self.apod_img_link = self.url + self.apod_img_name
+        self.apod_img_title = str(self.soup.find('b'))
+        self.today_name = self.apod_img_title[4:-5]
+        if ":" in self.today_name:
+            self.today_name = self.today_name.replace(":", " -", 1)
+        self.image_to_save = urllib.request.urlopen(self.apod_img_link).read()
+        return self.today_name, self.image_to_save
 
-def job():
-    save_name, image = get_image()
-    image_path = save_image(save_name, image)
-    ctypes.windll.user32.SystemParametersInfoW(20, 0, str(image_path), 0)
-    return
+    def save_image(self, name, image):
+        self.home_dir = os.path.expanduser('~')
+        self.today_date = str(datetime.date.today())
+        self.filename = self.today_date + " " + name + ".jpg"
+        self.save_path = os.path.join(self.home_dir, "Pictures", "APOD Photos")
+        self.save_path_with_img = os.path.join(self.save_path, self.filename)
+        self.normed_path = os.path.normpath(self.save_path)
+        self.normed_path_with_img = os.path.normpath(self.save_path_with_img)
+        if not os.path.exists(self.normed_path):
+            os.makedirs(self.normed_path)
+        with open(self.normed_path_with_img, 'wb',) as f:
+            f.write(image)
+        f.close()
+        return self.normed_path_with_img, self.today_date
+
+    def job(self):
+        self.save_name, self.image = self.get_image()
+        image_path = self.save_image(save_name, image)
+        ctypes.windll.user32.SystemParametersInfoW(20, 0, str(image_path), 0)
+        return
+
+set_background = DesktopBackground()
 
 print("Updating...")
 try:
-    today_name, image = get_image()
+    today_name, image = set_background.get_image()
     print("Today's image is of '{}'.".format(today_name))
-    image_path, today_date = save_image(today_name, image)
+    image_path, today_date = set_background.save_image(today_name, image)
     ctypes.windll.user32.SystemParametersInfoW(20, 0, str(image_path), 0)
     print("Image set as background.")
 except TypeError:
@@ -57,14 +61,14 @@ while True:
     formatted_time = datetime.datetime.strftime(current_time, "%H:%M:%S")
     print("The current time is {}".format(str(formatted_time)))
     try:
-        next_day_name, _ = get_image()
+        next_day_name, _ = set_background.get_image()
         if next_day_name == today_name:
             print("APOD image not changed from last check.")
             print("Waiting 10 minutes for next check.")
             time.sleep(600)
         else:
             print("Updating at {}".format(str(formatted_time)))
-            job()
+            set_background.job()
             time.sleep(43200)
             print("Waiting 12 hours before next check.")
     except TypeError:
