f3c950606e99df1f26d34d36e31b8380c643ff42
Nick <nickferguson107@icloud.com>
2021-03-15 16:53:21 +0000

    Removed OO-approach and tidied up

--- APOD_DB.py
+++ APOD_DB.py
@@ -6,71 +6,73 @@ import ctypes
 import sched
 from bs4 import BeautifulSoup
 
-class DesktopBackground:
+def get_image(url):  
 
-    def get_image(self):  
-        self.url = "http://apod.nasa.gov/"
-        self.page = urllib.request.urlopen(self.url)
-        self.soup = BeautifulSoup(self.page, "html.parser")
-        self.img = self.soup.find("img")
-        self.apod_img_name = str(self.img['src'])
-        self.apod_img_link = self.url + self.apod_img_name
-        self.apod_img_title = str(self.soup.find('b'))
-        self.today_name = self.apod_img_title[4:-5]
-        if ":" in self.today_name:
-            self.today_name = self.today_name.replace(":", " -", 1)
-        self.image_to_save = urllib.request.urlopen(self.apod_img_link).read()
-        return self.today_name, self.image_to_save
+    url = "http://apod.nasa.gov/"
+    page = urllib.request.urlopen(url)
+    soup = BeautifulSoup(page, "html.parser")
+    img = soup.find("img")
+    apod_img_name = str(img['src'])
+    apod_img_link = url + apod_img_name
+    apod_img_title = str(soup.find('b'))
+    today_name = apod_img_title[4:-5]
 
-    def save_image(self, name, image):
-        self.home_dir = os.path.expanduser('~')
-        self.today_date = str(datetime.date.today())
-        self.filename = self.today_date + " " + name + ".jpg"
-        self.save_path = os.path.join(self.home_dir, "Pictures", "APOD Photos")
-        self.save_path_with_img = os.path.join(self.save_path, self.filename)
-        self.normed_path = os.path.normpath(self.save_path)
-        self.normed_path_with_img = os.path.normpath(self.save_path_with_img)
-        if not os.path.exists(self.normed_path):
-            os.makedirs(self.normed_path)
-        with open(self.normed_path_with_img, 'wb',) as f:
-            f.write(image)
-        f.close()
-        return self.normed_path_with_img, self.today_date
+    if ":" in today_name:
+        today_name = today_name.replace(":", " -", 1)
 
-    def job(self):
-        self.save_name, self.image = self.get_image()
-        image_path = self.save_image(save_name, image)
-        ctypes.windll.user32.SystemParametersInfoW(20, 0, str(image_path), 0)
-        return
+    image_to_save = urllib.request.urlopen(apod_img_link).read()
 
-set_background = DesktopBackground()
+    return today_name, image_to_save
 
-print("Updating...")
-try:
-    today_name, image = set_background.get_image()
-    print("Today's image is of '{}'.".format(today_name))
-    image_path, today_date = set_background.save_image(today_name, image)
-    ctypes.windll.user32.SystemParametersInfoW(20, 0, str(image_path), 0)
-    print("Image set as background.")
-except TypeError:
-    print("Update failed - APOD item is video. Waiting 1 hour to update.")
-    time.sleep(3600)
+def save_image(name, image):
 
-while True:
-    current_time = datetime.datetime.now()
-    formatted_time = datetime.datetime.strftime(current_time, "%H:%M:%S")
-    print("The current time is {}".format(str(formatted_time)))
+    home_dir = os.path.expanduser('~')
+    today_date = str(datetime.date.today())
+    filename = today_date + " " + name + ".jpg"
+    save_path = os.path.join(home_dir, "Pictures", "APOD Photos")
+    save_path_with_img = os.path.join(save_path, filename)
+    normed_path = os.path.normpath(save_path)
+    normed_path_with_img = os.path.normpath(save_path_with_img)
+
+    if not os.path.exists(normed_path):
+        os.makedirs(normed_path)
+
+    with open(normed_path_with_img, 'wb',) as f:
+        f.write(image)
+    f.close()
+
+    return normed_path_with_img, today_date
+
+def main(url):
+
     try:
-        next_day_name, _ = set_background.get_image()
-        if next_day_name == today_name:
+        today_name, image = get_image(url)
+        print("Today's image is of '{}'.".format(today_name))
+        image_path, _ = save_image(today_name, image)
+        ctypes.windll.user32.SystemParametersInfoW(20, 0, str(image_path), 0)
+        print("Image set as background.")
+    except TypeError:
+        print("Update failed - APOD item is video. Waiting 1 hour to update.")
+        time.sleep(3600)
+
+    return today_name
+
+if __name__ == '__main__':
+
+    url = "http://apod.nasa.gov/"
+    today_name = main(url)
+
+    while 1:
+        current_time = datetime.datetime.now()
+        formatted_time = datetime.datetime.strftime(current_time, "%H:%M:%S")
+        print("The current time is {}".format(str(formatted_time)))
+        next_name, _ = get_image(url)
+        if next_name == today_name:
             print("APOD image not changed from last check.")
             print("Waiting 10 minutes for next check.")
             time.sleep(600)
         else:
             print("Updating at {}".format(str(formatted_time)))
-            set_background.job()
+            main(url)
             time.sleep(43200)
             print("Waiting 12 hours before next check.")
-    except TypeError:
-        print("Update failed - APOD item is video. Waiting 1 hour to update.")
-        time.sleep(3600)
---
+++ APOD_DB.pyw
@@ -0,0 +1,80 @@
+import urllib.request
+import datetime
+import time
+import os
+import ctypes
+import sched
+from bs4 import BeautifulSoup
+
+class DesktopBackground:
+
+    def get_image(self):  
+        self.url = "http://apod.nasa.gov/"
+        self.page = urllib.request.urlopen(self.url)
+        self.soup = BeautifulSoup(self.page, "html.parser")
+        self.img = self.soup.find("img")
+        self.apod_img_name = str(self.img['src'])
+        self.apod_img_link = self.url + self.apod_img_name
+        self.apod_img_title = str(self.soup.find('b'))
+        self.today_name = self.apod_img_title[4:-5]
+        if ":" in self.today_name:
+            self.today_name = self.today_name.replace(":", " -", 1)
+        self.image_to_save = urllib.request.urlopen(self.apod_img_link).read()
+        return self.today_name, self.image_to_save
+
+    def save_image(self, name, image):
+        self.home_dir = os.path.expanduser('~')
+        self.today_date = str(datetime.date.today())
+        self.filename = self.today_date + " " + name + ".jpg"
+        self.save_path = os.path.join(self.home_dir, "Pictures", "APOD Photos")
+        self.save_path_with_img = os.path.join(self.save_path, self.filename)
+        self.normed_path = os.path.normpath(self.save_path)
+        self.normed_path_with_img = os.path.normpath(self.save_path_with_img)
+        if not os.path.exists(self.normed_path):
+            os.makedirs(self.normed_path)
+        with open(self.normed_path_with_img, 'wb',) as f:
+            f.write(image)
+        f.close()
+        return self.normed_path_with_img, self.today_date
+
+    def job(self):
+        self.save_name, self.image = self.get_image()
+        self.image_path = self.save_image(self.save_name, self.image)
+        ctypes.windll.user32.SystemParametersInfoW(20, 0, str(self.image_path), 0)
+        return
+
+set_background = DesktopBackground()
+
+print("Updating...")
+try:
+    today_name, image = set_background.get_image()
+    print("Today's image is of '{}'.".format(today_name))
+    image_path, today_date = set_background.save_image(today_name, image)
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, str(image_path), 0)
+    print("Image set as background.")
+except TypeError:
+    print("Update failed - APOD item is video. Waiting 1 hour to update.")
+    time.sleep(3600)
+
+while True:
+    current_time = datetime.datetime.now()
+    formatted_time = datetime.datetime.strftime(current_time, "%H:%M:%S")
+    print("The current time is {}".format(str(formatted_time)))
+    try:
+        next_day_name, _ = set_background.get_image()
+        if next_day_name == today_name:
+            print("APOD image not changed from last check.")
+            print("Waiting 10 minutes for next check.")
+            time.sleep(600)
+        else:
+            print("Updating at {}".format(str(formatted_time)))
+            today_name, image = set_background.get_image()
+            print("Today's image is of '{}'.".format(today_name))
+            image_path, today_date = set_background.save_image(today_name, image)
+            ctypes.windll.user32.SystemParametersInfoW(20, 0, str(image_path), 0)
+            print("Image set as background.")
+            time.sleep(43200)
+            print("Waiting 12 hours before next check.")
+    except TypeError:
+        print("Update failed - APOD item is video. Waiting 1 hour to update.")
+        time.sleep(3600)
